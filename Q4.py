68b2801a07c9b708c1b63e1f36058a9577901e6b
naro143 <y.ishimi@itdharman.com>
2019-05-04 13:44:20 +0900

    add: ishimi file

--- Q4.py
+++ Q4.py
@@ -1,22 +1,19 @@
-total = int(input("買い物の合計金額を入力してください。:"))
-pay = int(input("支払う金額を入力してください。:"))
-otsuri = pay - total
-ichiman = otsuri // 10000
-gosen = (otsuri - (10000*ichiman)) // 5000
-sen = (otsuri - (10000*ichiman+5000*gosen)) // 1000
-gohyaku = (otsuri - (10000*ichiman+5000*gosen+1000*sen)) // 500
-hyaku = (otsuri - (10000*ichiman+5000*gosen+1000*sen+500*gohyaku)) // 100
-goju = (otsuri - (10000*ichiman+5000*gosen+1000*sen+500*gohyaku+100*hyaku)) // 50
-ju = (otsuri - (10000*ichiman+5000*gosen+1000*sen+500*gohyaku+100*hyaku+50*goju)) // 10
-go = (otsuri - (10000*ichiman+5000*gosen+1000*sen+500*gohyaku+100*hyaku+50*goju+10*ju)) // 5
-ichi = (otsuri - (10000*ichiman+5000*gosen+1000*sen+500*gohyaku+100*hyaku+50*goju+10*ju+5*go)) // 1
-print("お釣りは、"+str(otsuri)+"円で、")
-print("10000円札が、"+str(ichiman)+"枚、")
-print("5000円札が、"+str(gosen)+"枚、")
-print("1000円札が、"+str(sen)+"枚、")
-print("500円玉が、"+str(gohyaku)+"枚、")
-print("100円玉が、"+str(hyaku)+"枚、")
-print("50円玉が、"+str(goju)+"枚、")
-print("10円玉が、"+str(ju)+"枚、")
-print("5円玉が、"+str(go)+"枚、")
-print("1円玉が、"+str(ichi)+"枚です。")
+MONEY_KIND_LIST = [10000, 5000, 1000, 500, 100, 50, 10, 5, 1]
+print("買い物の合計金額を入力してください。")
+cost = int(input())
+print("支払う金額を入力してください。")
+pay = int(input())
+if pay < cost:
+    print("金額が足りません。")
+else:
+    change = cost - pay
+    print("お釣りは、{0}円で、".format(change))
+    for money in MONEY_KIND_LIST:
+        unit = ""
+        if money > 500:
+            unit = "札"
+        else:
+            unit = "玉"
+        print("{0}円{1}が{2}枚".format(money, unit, change // money))
+        change -= change // money * money
+    print("です。")
